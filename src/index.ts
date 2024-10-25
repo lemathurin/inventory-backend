68c6b877eed9dc857efdc38dc989b2b97f84420f
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-10-25 19:06:07 +0200

    origin changes

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,16 @@ dotenv.config();
 const app = express();
 const prisma = new PrismaClient();
 
+const allowedOrigins = ['http://78.47.140.225:3002', 'http://localhost:3002'];
+
 app.use(cors({
-  origin: ['http://78.47.140.225:3002', 'http://localhost:3002'],
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
